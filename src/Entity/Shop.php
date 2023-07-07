c3c14b0ad723c7f22c21271002d860f4224aa7a6
Salim <bouzidsezlim@gmail.com>
2023-07-07 17:00:12 +0200

    shop2

---
+++ src/Entity/Shop.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Entity;
+
+class Shop
+{
+
+
+    public function __construct(private string $name, private string $adress, private ?int $id = null)
+    {
+    }
+
+
+
+
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name 
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdress(): string
+    {
+        return $this->adress;
+    }
+
+    /**
+     * @param string $adress 
+     * @return self
+     */
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+        return $this;
+    }
+
+    /**
+     * @return 
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param  $id 
+     * @return self
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+}
---
+++ src/Repository/ShopRepository.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Shop;
+
+class ShopRepository
+{
+
+    public function findAll(): array
+    {
+        $list = [];
+        $connection = Database::getConnection();
+
+        $query = $connection->prepare("SELECT * FROM shop");
+
+        $query->execute();
+
+        foreach ($query->fetchAll() as $line) {
+            $list[] = new Shop($line["name"], $line["adress"], $line["id"]);
+
+        }
+
+        return $list;
+
+
+
+    }
+
+    public function findById(int $id): ?Shop
+    {
+
+        $connection = Database::getConnection();
+
+        $query = $connection->prepare("SELECT * FROM shop WHERE id=:id ");
+        $query->bindValue(":id", $id);
+        $query->execute();
+
+        foreach ($query->fetchAll() as $line) {
+            $shop = new Shop($line["name"], $line["adress"], $line["id"]);
+
+            return $shop;
+        }
+        return null;
+
+    }
+
+    public function persist(Shop $shop)
+    {
+        $connection = Database::getConnection();
+
+        $query = $connection->prepare("INSERT INTO monture (name,adress) VALUES (:name,:adress)");
+        $query->bindValue(':marque', $shop->getName());
+        $query->bindValue(':model', $shop->getAdress());
+
+
+
+        $query->execute();
+
+
+        $shop->setId($connection->lastInsertId());
+    }
+
+    public function delete(int $id)
+    {
+
+        $connection = Database::getConnection();
+
+        $query = $connection->prepare("DELETE FROM shop WHERE id=:id");
+        $query->bindValue(":id", $id);
+        $query->execute();
+    }
+
+    public function update(Shop $shop)
+    {
+
+        $connection = Database::getConnection();
+
+        $query = $connection->prepare("UPDATE monture SET marque=:marque, model=:model, basePrice=:basePrice,  picture=:picture WHERE id=:id");
+        $query->bindValue(':name', $shop->getName());
+        $query->bindValue(':adress', $shop->getAdress());
+
+
+        $query->bindValue(":id", $shop->getId());
+
+        $query->execute();
+    }
+
+
+
+
+
+
+
+}
